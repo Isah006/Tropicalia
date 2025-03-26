aea5759879232220f55554a0c8bfb76de1ef7706
Isah006 <159157061+Isah006@users.noreply.github.com>
2025-03-26 12:33:32 -0300

    26/03

--- script.js
+++ script.js
@@ -11,11 +11,11 @@ document.addEventListener('DOMContentLoaded', function(){
         tamanhoAtualFonte -=0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
     })
-    const botaoDeAcessibilidade = document.getElementById('botao-aacessibilidade');
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade');
     const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade');
     botaoDeAcessibilidade.addEventListener('click', function (){
-        botaoDeAcessibilidade.classList.toggle('totacao-botao');
-        botaoDeAcessibilidade.classList.toggle('apresenta-lista')
+        botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+        opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
     })
     
 });
