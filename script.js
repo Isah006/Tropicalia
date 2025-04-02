65f1cd9692fd45959b82790c7b9e31653dfe93b1
Isah006 <159157061+Isah006@users.noreply.github.com>
2025-04-02 12:25:07 -0300

    02/04

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function(){
     const aumentaFonteBotao=document.getElementById('aumentar-fonte')
     const diminuiFonteBotao=document.getElementById('diminuir-fonte')
+    const alternaContraste = document.getElementById('alterna-contraste')
     
     let tamanhoAtualFonte = 1;
     aumentaFonteBotao.addEventListener('click',function(){
@@ -16,6 +17,9 @@ document.addEventListener('DOMContentLoaded', function(){
     botaoDeAcessibilidade.addEventListener('click', function (){
         botaoDeAcessibilidade.classList.toggle('rotacao-botao');
         opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
+    })
+    alternaContraste.addEventListener ('click', function(){
+        document.body.classList.toggle('alto-contraste')
     })
     
 });
